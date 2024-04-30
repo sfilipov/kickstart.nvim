98cd1ee184d3f5f941b9c7e08097afc4ebfdd151
Simeon Filipov <ssfilipov@gmail.com>
2024-04-30 11:14:21 +0300

    remember last location in file

--- init.lua
+++ init.lua
@@ -861,6 +861,10 @@ require('lazy').setup({
     end,
   },
 
+  { -- Remember the last place in file when reopened
+    'farmergreg/vim-lastplace',
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
