49a5ec82264dd9339bf2e0fbeb92680fefb0fdc9
Simeon Filipov <ssfilipov@gmail.com>
2024-04-30 12:23:39 +0300

    basic bulgarian keymap config

--- init.lua
+++ init.lua
@@ -154,6 +154,10 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.opt.keymap = 'bulgarian-phonetic'
+vim.opt.iminsert = 0
+vim.opt.imsearch = -1
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
